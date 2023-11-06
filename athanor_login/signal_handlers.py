c8a1c81f060a225a6a527c9ca90a35b808ef65fd
Andrew Bastien <volundmush@gmail.com>
2023-11-06 16:02:22 -0500

    Initial commit.

--- athanor_login/signal_handlers.py
+++ athanor_login/signal_handlers.py
@@ -1,21 +1,50 @@
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR', None)
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]  # To get the first IP, if there are multiple IPs due to proxies.
+    else:
+        ip = request.META.get('REMOTE_ADDR', None)
+    return ip
+
+
+def django_login_success(sender, **kwargs):
+    if not (user := kwargs.get("user", dict())):
+        pass
+    if not (request := kwargs.get("request", None)):
+        pass
+    if not (ip := get_client_ip(request)):
+        pass
+    _login_record(user, ip)
+
+
+def django_login_fail(sender, **kwargs):
+    if not (request := kwargs.get("request", None)):
+        pass
+    if not (ip := get_client_ip(request)):
+        pass
+    if not (credentials := kwargs.get("credentials", dict())):
+        pass
+    if not (username := credentials.get("username", None)):
+        pass
+    from evennia import DefaultAccount
+    if not (account := DefaultAccount.objects.filter_family(username__iexact=username).first()):
+        pass
+    _login_record(account, ip, success=False, reason=kwargs.get("reason", "Failed to authenticate."))
+
 
 def login_success(sender, **kwargs):
     if not (session := kwargs.get("session", None)):
         return
-    ip = session.address
-
-    from .models import Host, Record
-
-    host, created = Host.objects.get_or_create(ip=ip)
-    host.records.create(user=sender, is_success=True)
+    _login_record(sender, session.address)
 
 
 def login_fail(sender, **kwargs):
     if not (session := kwargs.get("session", None)):
         return
-    ip = session.address
+    _login_record(sender, session.address, success=False, reason=kwargs.get("reason", "Failed to authenticate."))
 
+
+def _login_record(user, ip, success=True, reason=None):
     from .models import Host, Record
-
     host, created = Host.objects.get_or_create(ip=ip)
-    host.records.create(user=sender, is_success=False, reason="Failed to authenticate")
+    host.records.create(user=user, is_success=success, reason=reason)
--- athanor_login/startup_hooks.py
+++ athanor_login/startup_hooks.py
@@ -1,9 +1,12 @@
 def at_server_start():
     from evennia.server import signals
 
-    from . signal_handlers import login_success, login_fail
+    from . signal_handlers import login_success, login_fail, django_login_fail, django_login_success
 
     signals.SIGNAL_ACCOUNT_POST_LOGIN.connect(login_success)
     signals.SIGNAL_ACCOUNT_POST_LOGIN_FAIL.connect(login_fail)
 
+    from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 
+    user_logged_in.connect(django_login_success)
+    user_login_failed.connect(django_login_fail)
