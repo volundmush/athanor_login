23dd6574a443f5554ac612691eb0c713b8077e73
Andrew Bastien <volundmush@gmail.com>
2023-11-06 16:07:01 -0500

    Initial commit.

--- athanor_login/models.py
+++ athanor_login/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Host(models.Model):
     ip = models.GenericIPAddressField(unique=True)
-    hostname = models.CharField(max_length=255, unique=True)
+    hostname = models.CharField(max_length=255, null=True)
 
 
 class Record(models.Model):
